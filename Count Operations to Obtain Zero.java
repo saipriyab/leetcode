319ff526009e8140b8a1da00debeeb8a71c862d4
Saipriyadarshini Bandi <34882201+saipriyab@users.noreply.github.com>
2022-05-25 19:36:05 +0530

    Create Count Operations to Obtain Zero.java

---
+++ Count Operations to Obtain Zero.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int cnt=0;
+        while(num1>0&&num2>0)
+        {
+            if(num1>num2)
+            {
+                num1=num1-num2;
+                cnt++;
+            }
+            else
+            {
+                num2=num2-num1;
+                cnt++;
+            }
+            if(num1<0||num2<0)
+                break;
+        }
+        return cnt;
+    }
+}
+
+leetcode url: https://leetcode.com/problems/count-operations-to-obtain-zero/
