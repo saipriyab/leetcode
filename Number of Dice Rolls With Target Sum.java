2f8935d4d8ad080f956366f2d4965306880684e7
Saipriyadarshini Bandi <34882201+saipriyab@users.noreply.github.com>
2022-10-02 16:34:14 +0530

    Create Number of Dice Rolls With Target Sum.java

---
+++ Number of Dice Rolls With Target Sum.java
@@ -0,0 +1,19 @@
+url: https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/
+
+//Bruteforce approach
+class Solution {
+    public int numRollsToTarget(int n, int k, int target) {
+        int MOD=1000000007;
+        if(target<n||target>(n*k))
+            return 0;
+        if(n==1)
+            return (target<=k)?1:0;
+        int sum=0;
+        for(int i=1;i<=k;i++)
+        {
+            sum+=numRollsToTarget(n-1,k,target-i);
+            sum%=MOD;
+        }
+        return sum;
+    }
+}
