b531eb6c26179e6457c5e5a944bd99228cf79050
Saipriyadarshini Bandi <34882201+saipriyab@users.noreply.github.com>
2022-12-03 22:46:06 +0530

    Update Determine if String Halves Are Alike.java

--- Determine if String Halves Are Alike.java
+++ Determine if String Halves Are Alike.java
@@ -1,6 +1,6 @@
 leetcode url: https://leetcode.com/problems/determine-if-string-halves-are-alike/
 
-/*Approach*/
+/**Approach 1**/
 class Solution {
     public boolean halvesAreAlike(String s) {
         s=s.toLowerCase();
@@ -13,3 +13,21 @@ class Solution {
     }
 }
 
+/**Approach 2**/
+class Solution {
+    public boolean halvesAreAlike(String s) {
+       Set<Character> vowels=Set.of('a','e','i','o','u','A','E','I','O','U');
+        int cnt=0,mid=s.length()/2;
+        char a='0',b='0';
+        for(int i=0;i<s.length()/2;i++)
+        {
+           a=s.charAt(i);
+           b=s.charAt(mid+i);
+            if(vowels.contains(a))
+                cnt++;
+            if(vowels.contains(b))
+               cnt--;
+        }
+        return cnt==0;
+    }
+}
