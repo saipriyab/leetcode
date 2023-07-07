97216eff2941a430e28a591a53bc5975b49b3d7b
Saipriyadarshini Bandi <34882201+saipriyab@users.noreply.github.com>
2023-07-07 20:24:59 +0530

    Update Maximum Product Difference Between Two Pairs.java

--- Maximum Product Difference Between Two Pairs.java
+++ Maximum Product Difference Between Two Pairs.java
@@ -1,3 +1,4 @@
+//Brtue Force
 class Solution {
     public int maxProductDifference(int[] nums) {
         Arrays.sort(nums);
@@ -5,5 +6,35 @@ class Solution {
         int min=nums[0]*nums[1];
         return max-min;
         
+    }
+}
+
+//Optimal Solution
+class Solution {
+    public int maxProductDifference(int[] nums) {
+      int max1=Integer.MIN_VALUE,max2=Integer.MIN_VALUE,
+      min1=Integer.MAX_VALUE,min2=Integer.MIN_VALUE;
+      for(int i=0;i<nums.length;i++)
+      {
+          if(nums[i]>=max1)
+          {
+              max2=max1;
+              max1=nums[i];
+          }
+         if(nums[i]>=max2&&nums[i]<max1)
+          {
+              max2=nums[i];
+          }
+             if(nums[i]<=min1)
+          {
+              min2=min1;
+              min1=nums[i];
+          }
+          if(nums[i]<min2&&nums[i]>min1)
+          {
+              min2=nums[i];
+          }
+      }
+        return (max2*max1)-(min2*min1);
     }
 }
