28d60cc616b37a47ebc80bb8579cd11bc4a4f8a7
Saipriyadarshini Bandi <34882201+saipriyab@users.noreply.github.com>
2023-07-08 13:37:48 +0530

    Update Apply Operations to an Array.java

--- Apply Operations to an Array.java
+++ Apply Operations to an Array.java
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/apply-operations-to-an-array/description/
 
+//using extra array
 class Solution {
     public int[] applyOperations(int[] nums) {
         int i=0,j=0;
@@ -28,3 +29,37 @@ class Solution {
          return result;
     }
 }
+
+
+
+//without using extra array
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int i=0,index=0;
+         for(i=0;i<nums.length-1;i++)
+         {
+             if(nums[i]==nums[i+1])
+             {
+                 nums[i]=(nums[i]*2);
+                  nums[i+1]=0;
+             }
+             
+         }
+         
+         for(i=0;i<nums.length;i++)
+         {
+             if(nums[i]!=0)
+             {
+                 nums[index]=nums[i];
+                 index++;
+                
+             }
+         }
+         for(i=index;i<nums.length;i++)
+         {
+             nums[i]=0;
+         }
+         return nums;
+    }
+}
+
